aea93c922b211db74bc2ab31172c2ffd8a77e407
MaksZ <makim.z.2000@mail.ru>
2021-03-11 20:52:36 +0300

    rm raz.sh, upd init: step 2 - format device...

--- init.sh
+++ init.sh
@@ -32,6 +32,7 @@ Args:
   -m, --mount     Mountpoint
   -s, --step      Start from step
 Steps:
+  1) localization
   1) format device and mount partitions
   2) install base packages and WM
   3) generate /etc/fstab
@@ -84,13 +85,36 @@ done
 
 
 
+if [[ $step -le 1 ]]
+then
+  echo '########################################################################'
+  echo '#                                                                      #'
+  echo '# Step 1: localization                                                 #'
+  echo '#                                                                      #'
+  echo '########################################################################'
+  echo
+
+  loadkeys ru
+  setfont cyr-sun16
+  # nano /etc/locale.gen     ####  add   ru_RU.UTF-8 UTF-8
+  locale-gen
+  export LANG="ru_RU.UTF-8"
+
+  echo 'Completed!'
+  step=2
+fi
+
+
+
+
+
 
 
 if [[ $step -le 1 ]]
 then
   echo '########################################################################'
   echo '#                                                                      #'
-  echo '# Step 1: format device and mount partitions                           #'
+  echo '# Step 2: format device and mount partitions                           #'
   echo '#                                                                      #'
   echo '########################################################################'
   echo
@@ -100,39 +124,141 @@ then
 
   # РАЗМЕТКА
   # ----------------------------------------------------------
-  parted -a optimal $device  	# выравнивание разделов
-  unit mib
-  mklabel gpt
+  (
+  echo 'unit mib';
+  echo 'mklabel gpt';
+  echo 'mkpart primary 2 258';		#1 /dev/vda1 256MB EFI (FAT32)
+  echo 'set 1 boot on';
+  echo 'mkpart primary 258 514';		#2 /dev/vda2 256MB boot (ext2)
+  echo 'name 2 boot';
+  echo 'mkpart primary 514 4610';		#3 /dev/vda3 4GB (SWAP)
+  echo 'name 3 swap';
+  echo 'mkpart primary 4610 -1';		#4 /dev/vda4 (ext4)
+  echo 'name 4 root';
+  print
 
-  mkpart primary 2 258		#1 /dev/sda1 256MB EFI (FAT32)
-  set 1 boot on
-
-  mkpart primary 258 514	#2 /dev/sda2 256MB boot (ext2)
-  name 2 boot
-
-  mkpart primary 514 4610	#3 /dev/sda3 4GB (SWAP)
-  name 3 swap
-
-  mkpart primary 4610 -1	#4 /dev/sda4 (ext4)
-  name 4 root
-
-  print						#чё вышло?
-  quit
+  ) | parted -a optimal $device  # выравнивание разделов
 
   # ФОРМАТИРОВАНИЕ
-  # -----------------------------------------
-  mkfs.fat -F32 /dev/sda1		#EFI  (FAT32)
-  mkfs.ext2 /dev/sda2			#boot (ext2)
-  mkfs.ext4 /dev/sda4			#root (ext4)
-  # mkswap /dev/sda3			#swap
-  # swapon /dev/sda3
+  # ----------------------------------------------------------
+  mkfs.fat -F32 ${device}1	&&		#EFI  (FAT32)
+  mkfs.ext2 ${device}2 &&			#boot (ext2)
+  mkfs.ext4 ${device}4 &&			#root (ext4)
+  mkswap ${device}3 &&			#swap
+  swapon ${device}3 &&
+  # cfdisk /dev/vda
+  
 
   # МОНТИРОВАНИЕ
   # ---------------------------
-  mount /dev/sda4 /mnt
+  mount ${device}4 /mnt
   mkdir -p /mnt/{home,boot}
-  mount /dev/sda2 /mnt/boot
+  mount ${device}2 /mnt/boot
 
   echo 'Completed!'
-  step=2
+  step=3
+fi
+
+
+
+
+
+if [[ $step -le 2 ]]
+then
+  echo '########################################################################'
+  echo '#                                                                      #'
+  echo '# Step 3: install base packages                                        #'
+  echo '#                                                                      #'
+  echo '########################################################################'
+  echo
+
+  packages=(
+  	# База
+    linux-firmware
+    linux-zen-headers
+    linux-zen
+    base
+    base-devel
+    nano 
+    vim 
+    dhcpcd 
+    dialog 
+    wpa_supplicant 
+    netctl 
+    mc 
+    git 
+    wget
+    net-tools
+    htop
+
+    # Для ядра и загрузчика
+    grub 
+    efibootmgr 
+    dosfstools 
+    os-prober 
+    mtools
+
+
+    # Из интеренета
+    btrfs-progs
+    docker-compose
+    efibootmgr
+    fd
+    fzf
+    gimp
+    gparted
+    grub
+    gvm
+    jq
+    libldm
+    linux
+    linux-headers
+    man-db
+    man-pages
+    mc
+    mlocate
+    mpv
+    neofetch
+    networkmanager-openvpn
+    networkmanager-pptp
+    noto-fonts
+    noto-fonts-emoji
+    ntfs-3g
+    pkgfile
+    pass
+    qt5ct
+    reflector
+    rsync
+    snapper
+    systemd-swap
+    whois
+    xorg
+    zsh
+  )
+
+  set +e
+  pacstrap "$mount" "${packages[@]}"
+  set -e
+
+  echo 'Completed!'
+fi
+
+
+
+
+
+
+
+
+if [[ $step -le 3 ]]
+then
+  echo '########################################################################'
+  echo '#                                                                      #'
+  echo '# Step 4: generate /etc/fstab                                          #'
+  echo '#                                                                      #'
+  echo '########################################################################'
+  echo
+
+  genfstab -U "$mount" >> "$mount/etc/fstab"
+  echo 'Completed!'
 fi
